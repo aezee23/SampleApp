3d1d11df8013105c6b2b25d9f79fc981d72595bd
aezee23 <aezeanaka@gmail.com>
2016-03-14 07:16:50 +0000

    Added church name to data download

--- app/models/record.rb
+++ app/models/record.rb
@@ -38,7 +38,7 @@ def self.to_csv(options = {})
   CSV.generate(options) do |csv|
     csv << column_names
     all.each do |record|
-      csv << record.attributes.values_at(*column_names)
+      csv << record.attributes.values_at(*column_names).insert(-1, record.user.name)
     end
   end
 end
