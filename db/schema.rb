9971b14ac621664bef7f8c05231ddadf643bcef8
aezee23 <aezeanaka@gmail.com>
2016-03-15 20:00:20 +0000

    reset sent at

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308214702) do
+ActiveRecord::Schema.define(version: 20160315195957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160308214702) do
     t.datetime "updated_at",                      null: false
     t.string   "remember_digest"
     t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["church_group_id"], name: "index_users_on_church_group_id", using: :btree
