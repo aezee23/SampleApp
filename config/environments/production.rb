7ef48b00d0c451a2b44db1f01db254a0a7cb236f
aezee23 <aezeanaka@gmail.com>
2016-03-15 20:11:31 +0000

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ config.force_ssl = true
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'nameless-escarpment-37929.herokuapp.com'}
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
