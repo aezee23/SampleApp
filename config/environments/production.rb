ca61c187c6b95ee713ca865e85071ab954a911bb
aezee23 <aezeanaka@gmail.com>
2016-03-15 20:03:47 +0000

    syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ config.force_ssl = true
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => ENV['EMAIL']
- :password             => ENV['PASSWORD']
- :authentication       => "plain"
+ :user_name            => ENV['EMAIL'],
+ :password             => ENV['PASSWORD'],
+ :authentication       => "plain",
 :enable_starttls_auto => true
 }
 end
