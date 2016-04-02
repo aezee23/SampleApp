9c3554a3246295d16f871c2b70d0e2879e339e08
aezee23 <aezeanaka@gmail.com>
2016-04-02 22:22:13 +0100

    typo

--- app/controllers/church_groups_controller.rb
+++ app/controllers/church_groups_controller.rb
@@ -46,7 +46,7 @@ end
 
 def destroy
     ChurchGroup.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = "Church Group Deleted"
     redirect_to users_url
   end
 
