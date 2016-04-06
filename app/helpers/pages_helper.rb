cdf055d662d03b63c1967cac5afdcc8d19a93a6f
aezee23 <aezeanaka@gmail.com>
2016-04-06 06:39:45 +0100

    weekday table

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,4 +5,10 @@ module PagesHelper
 	b= a.select{|x| x.wday==0}
 	b
 	end
+
+      def thu_in_month(month)
+  a= Date.parse(month)..((Date.parse(month)>>1))-1
+  b= a.select{|x| x.wday==4}
+  b
+  end
 end
