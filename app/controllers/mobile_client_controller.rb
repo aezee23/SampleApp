8c9c88902a89c9329432b5a00db7a79fe6a170a5
aezee23 <aezeanaka@gmail.com>
2017-01-18 14:29:27 +0000

    bug fix

--- app/controllers/mobile_client_controller.rb
+++ app/controllers/mobile_client_controller.rb
@@ -29,7 +29,7 @@ class MobileClientController < ApplicationController
       next_month = (Date.parse(month) >> 1).strftime("%b%Y")
       results[month] = totals(records, month, next_month)
     end
-    results["Latest Data"] = totals(records.select{ |record| record.day == date_of_last("Sunday") }, months[1], months[0] )
+    results["Latest Data"] = totals(records.select{ |record| record.day == date_of_last("Sunday") }, months[0], (Date.parse(months[0]) >> 1).strftime("%b%Y") )
     results["Latest Data"]["month"] = "Latest Data"
     results["Latest Data"]["date"] = date_of_last("Sunday").strftime("%d%b%y")
 
