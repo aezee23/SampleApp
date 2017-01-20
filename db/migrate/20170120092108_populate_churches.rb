e54636da7542068f7855fa3e1263e93712dae99a
aezee23 <aezeanaka@gmail.com>
2017-01-20 18:26:35 +0000

    user id

--- db/migrate/20170120092108_populate_churches.rb
+++ db/migrate/20170120092108_populate_churches.rb
@@ -2,7 +2,7 @@ class PopulateChurches < ActiveRecord::Migration
   def change
   end
   User.where(admin: false, is_leader: false).each do |user|
-    Church.create(user: user, 
+    Church.create(user_id: user.id, 
       church_group_id: user.church_group_id,
       name: user.name,
       short_name: user.name.gsub(" ", "")[0..2].upcase,
