441bb9fa25d630c02cc1448afeeef8f4e17b3134
aezee23 <aezeanaka@gmail.com>
2017-01-20 18:48:18 +0000

    leader

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120162502) do
+ActiveRecord::Schema.define(version: 20170120184237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "church_groups", force: :cascade do |t|
     t.string   "name"
-    t.string   "leader"
     t.string   "region"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
