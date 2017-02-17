d5c1f09ed095c6a2f54a316dd2aa8e6b4d236254
aezee23 <aezeanaka@gmail.com>
2017-02-17 15:29:39 +0000

    church indexes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216013354) do
+ActiveRecord::Schema.define(version: 20170217140500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170216013354) do
     t.boolean  "sunday_meeting"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   create_table "members", force: :cascade do |t|
